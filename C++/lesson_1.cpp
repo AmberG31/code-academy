c1d37c369502e95ecdb8721490deac0b6fd3190a
AmberG31 <gynteee@yahoo.co.uk>
2023-07-13 19:50:53 +0300

    Lesson 1 completed

--- C++/lesson_1.cpp
+++ C++/lesson_1.cpp
@@ -3,3 +3,27 @@ int main() {
   std::cout << "Hello World!\n";
 
 }
+
+int main() {
+  std::cout << "Codecademy\n";
+}
+
+// excersise 5 
+int main() 
+{
+  
+  std::cout << "      1\n";
+  std::cout << "    2 3\n";
+  std::cout << "  4 5 6\n";
+  std::cout << "7 8 9 10\n";
+}
+
+// final excersise
+int main() 
+{
+  
+  std::cout << "Dear Self,\n";
+  std::cout << "My aim is to learn C++ and land a great job";
+  std::cout << "Amber, 13/07/2023";
+
+}

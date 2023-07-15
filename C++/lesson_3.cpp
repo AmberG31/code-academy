17c447b9451669999344cc3515a6637e30fdd519
AmberG31 <gynteee@yahoo.co.uk>
2023-07-15 20:30:46 +0300

    Arithmetic Operators

---
+++ C++/lesson_3.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+// int main() {
+  
+//   // Declare a variable   
+//   int year = 2019;
+  
+//   // Initialize a variable
+  
+   
+// }
+
+#include <iostream>
+
+int main() {
+
+  int score = 0;
+
+  // Change score here:
+  
+  score = 1234 * 99;
+  
+std::cout<<score<<"\n";
+
+}
+//  in the terminal: ->>>>>
+// C++ git:(main) ✗ g++ lesson_3.cpp -o score
+// ➜  C++ git:(main) ✗ ./score                  
+// 122166

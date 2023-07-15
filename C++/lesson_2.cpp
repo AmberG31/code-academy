03ec38b5dc7094bc1f8e0ae81dd93a9421bc1b33
AmberG31 <gynteee@yahoo.co.uk>
2023-07-15 20:07:35 +0300

    Compile and Execute wiht the terminal

---
+++ C++/lesson_2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() 
+{
+  
+  std::cout << "Hello World!\n";
+  
+}
+
+// to execute the comand and create a file with a new name need to type this to the terminal
+// $ g++ hello.cpp -o hello
+// $ ./hello
+// Hello World!

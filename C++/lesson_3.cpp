a75a4af330c35b3b2f1db7702633cf0de1fb8590
AmberG31 <gynteee@yahoo.co.uk>
2023-07-17 15:56:36 +0300

    User input

--- C++/lesson_3.cpp
+++ C++/lesson_3.cpp
@@ -1,16 +1,14 @@
 #include <iostream>
 
-// int main() {
+int main() {
   
-//   // Declare a variable   
-//   int year = 2019;
+  // Declare a variable   
+  int year = 2019;
   
-//   // Initialize a variable
+  // Initialize a variable
   
-   
-// }
+}
 
-#include <iostream>
 
 int main() {
 
@@ -36,6 +34,25 @@ int main() {
   // Output
   std::cout << "Player score: " << score << "\n";
 }
+//  in the terminal: ->>>>>
 // $ g++ game.cpp
 // $ ./a.out
 // Player score: 0
+
+//User input
+int main() {
+  
+  int tip = 0;
+  
+  std::cout << "Enter tip amount: ";
+  
+  std::cin >> tip;
+  
+  std::cout << "You paid " << tip << " dollars." << "\n";
+}
+
+//  in the terminal: ->>>>>
+// $ g++ tip.cpp
+// $ ./a.out
+// Enter tip amount: 5
+// You paid 5 dollars.

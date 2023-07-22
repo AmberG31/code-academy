3bcc65e53276e6d25e8de9b06b0fd62260672d1d
AmberG31 <gynteee@yahoo.co.uk>
2023-07-22 17:10:48 +0300

    Logical operator '||'

--- C++/lesson_5/logical_operators.cpp
+++ C++/lesson_5/logical_operators.cpp
@@ -5,9 +5,17 @@ int main() {
   int hunger = true;
   int anger = true;
   
-  // Write the code below:
   if (hunger && anger) {
     std::cout << "Hangry";
   }
   
+}
+
+int main() {
+  
+  int day = 6;
+    
+    if (day == 6 || day == 7) {
+     std::cout << "Weekend";
+    }
 }

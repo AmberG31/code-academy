cedf36b85362adfd125b8010905c0e395a9716d3
AmberG31 <gynteee@yahoo.co.uk>
2023-07-22 19:34:25 +0300

    Updated leapYear statement

--- C++/lesson_5/challenge_ leap_year.cpp
+++ C++/lesson_5/challenge_ leap_year.cpp
@@ -14,6 +14,8 @@ bool leapYear(int year) {
         return true;
     } else if (year % 100 == 0) {
         return true;
+    } else if (year % 100 == 0 &&  year % 400 != 0) {
+        return false;
     } else {
         return false;
     }
